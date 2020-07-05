491c2b5973ea6cb88ab8831cb1653b1f0df468fa
kjirou <sorenariblog@gmail.com>
2020-07-05 12:15:50 +0900

    Add a base function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-// TODO: Delete these.
-export type X = number
-export const x = 1
+export type HttpMethod = 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT'
+
+export function sendHttpRequestWithXhr(
+  httpMethod: HttpMethod,
+  url: string,
+  options: {
+    body?: string,
+    headers?: {[key in string]: string},
+  } = {},
+): Promise<{
+    xhr: XMLHttpRequest,
+  }> {
+  return new Promise(resolve => {
+    const xhr = new XMLHttpRequest()
+    xhr.onload = () => {
+      resolve({xhr})
+    }
+    xhr.open(httpMethod, url)
+    const headers = options.headers || {}
+    Object.keys(headers).sort().forEach((key) => {
+      xhr.setRequestHeader(key, headers[key])
+    })
+    xhr.send(options.body !== undefined ? options.body : null)
+  })
+}
